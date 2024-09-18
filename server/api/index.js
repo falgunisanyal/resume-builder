4dec4b2e159d4b7d044c6de789b5fdb77b202dda
Falguni Sanyal <falguni@i-motor.com.au>
2024-09-18 20:46:45 +1000

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,18 @@
-module.exports = (req, res) => {
-	const name = req.query.name || 'stranger';
-	res.status(200).send(`Hello, ${name}!`)
-}
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Hello world",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
